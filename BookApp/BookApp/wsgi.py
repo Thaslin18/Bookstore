589d3223b5e64c48d63092f344ffb14871d50786
Thaslin <thaslin18parveen@gmail.com>
2026-07-28 20:50:36 +0530

    Fix WSGI sys path and Vercel entrypoint

--- BookApp/BookApp/wsgi.py
+++ BookApp/BookApp/wsgi.py
@@ -1,17 +1,13 @@
-"""
-WSGI config for BookApp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add current working directory to Python module search path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookApp.settings')
 
 application = get_wsgi_application()
+
+# Required entrypoint variable for Vercel Serverless Functions
 app = application
